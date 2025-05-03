5808d9cbf2d68e24750059f11360908d20558964
csmaxi <csmaxinro@gmail.com>
2025-05-03 16:55:27 -0300

    domains

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'supabase.co', 'ggprifdqvrpuhbgbxahz.supabase.co'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'https',
+        hostname: 'supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ggprifdqvrpuhbgbxahz.supabase.co',
+      },
+    ],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  },
+  }
+  ,
   compress: true,
   poweredByHeader: false,
   reactStrictMode: true,
